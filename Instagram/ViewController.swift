ef779439844484308ad7912ce1b67b76f4a0bf7a
varolaksoy <pixtur@icloud.com>
2018-05-01 18:22:42 +0300

    parse bağlantısı yapıldı ve ilk kayıtlar yapıldı. tabi appdelegate.swift dosyasını mahrem bilgiler içerdiği için gönderemiyorum. :)

--- Instagram/ViewController.swift
+++ Instagram/ViewController.swift
@@ -7,12 +7,31 @@
 //
 
 import UIKit
+import Parse
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
+        
+        
+        
+        let gameScore = PFObject(className:"GameScore")
+        gameScore["score"] = 1337
+        gameScore["playerName"] = "Sean Plott"
+        gameScore["cheatMode"] = false
+        gameScore.saveInBackground { (success, error) in
+            if (success) {
+                print ("başarılı!")
+            } else {
+                print("başarısız!")
+            }
+        }
+
     }
 
     override func didReceiveMemoryWarning() {
