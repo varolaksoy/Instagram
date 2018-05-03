390f2b28d3bc1c0cd1625d0bd2db170369a0a733
varolaksoy <pixtur@icloud.com>
2018-05-03 16:10:23 +0300

    imageview görüntüleme, alert kullanımı gibi şeyler eklendi, yorum satırı haline getirildi. belki ileride işe yarar, sakla samanı, gelir zamanı.

--- Instagram/ViewController.swift
+++ Instagram/ViewController.swift
@@ -9,11 +9,52 @@
 import UIKit
 import Parse
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
 
+    @IBAction func chooseImage(_ sender: Any) {
+        
+        //buton ile seçilen resmi imageview'de gösterme.
+//        let imagePickerController = UIImagePickerController()
+//        imagePickerController.delegate = self
+//        imagePickerController.sourceType = UIImagePickerControllerSourceType.photoLibrary
+//        imagePickerController.allowsEditing = false
+//        self.present(imagePickerController, animated: true, completion: nil)
+        
+       
+        // alert tepkisi
+//        let alertController = UIAlertController(title: "hey there!", message: "are you sure?", preferredStyle: UIAlertControllerStyle.alert)
+//        alertController.addAction((UIAlertAction(title: "ok", style: .default, handler: { (action) in
+//            print("ok button pressed")
+//        })))
+//
+//        alertController.addAction(UIAlertAction.init(title: "no", style: .default, handler: { (action) in
+//            print("no button pressed")
+//            self.dismiss(animated: true, completion: nil)
+//        }))
+//        self.present(alertController, animated: true, completion: nil)
+   }
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+//    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+//        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+//            imageView.image = image
+//        } else {
+//            print("there was a problem getting the image")
+//        }
+//        self.dismiss(animated: true, completion: nil)
+//    }
+    
+    @IBAction func pauseApp(_ sender: Any) {
+        
+        
+    }
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         
         
         
@@ -41,6 +82,18 @@ class ViewController: UIViewController {
 //                }
 //            }
 //        }
+        
+        //tweet ekleme
+//        let tweet = PFObject(className: "tweet")
+//        tweet["text"] = "Bu benim ilk tweetim!"
+//        tweet.saveInBackground { (succes, error) in
+//            if (succes) {
+//                print("tweet gönderildi")
+//            } else  {
+//                print("bi sıkıntı var galiba")
+//            }
+//        }
+        
     }
 
 }
